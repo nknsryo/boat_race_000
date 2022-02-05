9c39f62a69aa9f84798ad5f34a86588520587762
nknsryo <nkns.ryo@gmail.com>
2022-02-05 23:56:42 +0900

    スクレイピングでけたよー！！！！！！

--- place_data.py
+++ place_data.py
@@ -39,7 +39,6 @@ driver = webdriver.Chrome(options=chromedriver_options())
 
 for race_place in range(1, 25):
     time.sleep(3)
-    race_info = []
     driver.get(f"https://kyoteibiyori.com/index.php?hiduke={todays_date()}")
     driver.implicitly_wait(5)
     x = "中止順延"
@@ -56,6 +55,48 @@ for race_place in range(1, 25):
     else:
         place_name = driver.find_element(By.XPATH,
                                          f"/html/body/div[4]/div/section[1]/div[2]/ul/li[{race_place}]/a/div[1]").text
+    for race_number in range(1, 13):
+        race_info = []
         race_info.append(place_name)
-        
+        time.sleep(2)
+        driver.get(f"https://kyoteibiyori.com/race_shusso.php?"
+                   f"place_no=2&race_no={race_number}&hiduke={todays_date()}&slider=1")
+        driver.implicitly_wait(5)
+        driver.find_element(By.XPATH, "/html/body/div[8]/div[1]/section/div[5]/div[1]/div/ul/li[2]").click()
+        driver.implicitly_wait(3)
+        first_win_rate = driver.find_element(By.XPATH,
+                                             "/html/body/div[8]/div[1]/section/div[5]/table[1]/tbody/tr[1]/td").text
+        race_info.append(f"{race_number}R")
+        race_info.append(first_win_rate)
+        for first_win_rate_player in range(1, 7):
+            first_win_rate_each = driver.find_element(By.XPATH,
+                                                      f"/html/body/div[8]/div[1]/section/div[5]"
+                                                      f"/table[1]/tbody/tr[4]/td[{first_win_rate_player + 1}]").text
+            race_info.append(first_win_rate_each)
+        driver.implicitly_wait(2)
+        second_in_rate = driver.find_element(By.XPATH,
+                                             f"/html/body/div[8]/div[1]/section/div[5]"
+                                             f"/table[1]/tbody/tr[6]/td").text
+        race_info.append(second_in_rate)
+
+        for second_in_rate_player in range(1, 7):
+            second_in_rate_each = driver.find_element(By.XPATH,
+                                                      f"/html/body/div[8]/div[1]/section/div[5]"
+                                                      f"/table[1]/tbody/tr[9]/td[{second_in_rate_player + 1}]").text
+            race_info.append(second_in_rate_each)
+
+        determination_way = driver.find_element(By.XPATH,
+                                                f"/html/body/div[8]/div[1]/section/div[5]"
+                                                f"/table[1]/tbody/tr[26]/td").text
+        race_info.append(determination_way)
+        escape_rate = driver.find_element(By.XPATH, f"/html/body/div[8]/div[1]/section/div[5]"
+                                                    f"/table[1]/tbody/tr[29]/td[1]").text
+        escaped_rate = driver.find_element(By.XPATH, f"/html/body/div[8]/div[1]/section/div[5]"
+                                                     f"/table[1]/tbody/tr[29]/td[2]").text
+        race_info.append(escape_rate)
+        race_info.append(escaped_rate)
+        driver.implicitly_wait(5)
+
+        print(race_info)
+        # ここにCSVデータ出力のプログラムを入れる
 driver.close()
