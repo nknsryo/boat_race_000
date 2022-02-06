4bcd0d5ea6b0bb991cd2f549a307c7c14d167873
nknsryo <nkns.ryo@gmail.com>
2022-02-06 23:29:10 +0900

    データベース作るよー

---
+++ db.py
@@ -0,0 +1,47 @@
+import os
+
+import psycopg2 as psycopg2
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+def get_connection():
+    """コネクションを貼る関数"""
+    dsn = os.environ.get("DATABASE_URL")
+    return psycopg2.connect(dsn)
+
+
+def cursor_execute(sql, params=()):
+    """sqlを実行する関数 select文なら実行結果を返す"""
+    with get_connection() as conn:
+        with conn.cursor() as cur:
+            # cur.execute(sql)
+            cur.execute(sql, params)
+            if sql.split(" ")[0] == "SELECT":
+                customers = cur.fetchall()
+                return customers
+
+
+def init_db():
+    """データベースの初期化を行う関数"""
+    with open('schema.sql', encoding="utf-8") as f:
+        cursor_execute(f.read())
+
+
+def get_all_customers():
+    sql = "SELECT * FROM customers;"
+    customers = cursor_execute(sql)
+    return customers
+
+
+def add_customer(name, age):
+    sql = "INSERT INTO customers VALUES (%(name)s, %(age)s);"
+    params = {"name": name, "age": age}
+    cursor_execute(sql, params)
+
+
+if __name__ == '__main__':
+    init_db()
+
+    print(init_db.__doc__)
---
+++ schema.sql
@@ -0,0 +1,35 @@
+
+
+-- もしテーブルが存在したら削除
+DROP TABlE IF EXISTS all_race_data;
+
+-- もしテーブルがなかったら作成  customers--テーブル名
+CREATE TABLE IF NOT EXISTS all_race_data(
+    data TEXT,
+    place_name TEXT,
+    race_number INTEGER,
+    one_3month_1win REAL,
+    two_3month_1win REAL,
+    three_3month_1win REAL,
+    four_3month_1win REAL,
+    five_3month_1win REAL,
+    six_3month_1win REAL,
+    one_3month_2win REAL,
+    two_3month_2win REAL,
+    three_3month_2win REAL,
+    four_3month_2win REAL,
+    five_3month_2win REAL,
+    six_3month_2win REAL,
+    one_6month_escape REAL,
+    two_6month_escaped REAL,
+);
+
+--  テストデータの挿入
+INSERT INTO
+    -- customers(name, age)とかにするとカラム指定ができる。
+    customers
+VALUES
+    ('Bob', 15),
+    ('Tom', 57),
+    ('Ken', 76)
+    ;
